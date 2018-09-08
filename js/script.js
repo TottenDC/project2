e10c2cd0614cfdd4a98cdff8bf4c28f7d3954f38
Daniel Totten <TottenDC@gmail.com>
2018-09-08 12:52:21 -0400

    Created show/hide student function
    Tested

--- js/script.js
+++ js/script.js
@@ -4,12 +4,28 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
+const arrayOfStudents = document.querySelectorAll('.student-item');
 
 
-
-// Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
-
+/*
+Show/Hide Student function
+Parameters: an array of objects containing student information / the current page number
+1. Loop to hide from DOM
+2. Display only 10 students based on page # (Ex: page 3 is (i=20; i<=29; i=i+1))
+  - If statement avoids JS error when i > array.length
+*/
+function displayStudents (studentList, page) {
+  for (i=0; i<studentList.length; i++) {
+    studentList[i].style.display = 'none';
+  }
+  for (i=((page*10)-10); i<=((page*10)-1); i++) {
+    if (studentList[i] === undefined) {
+      break;
+    } else {
+      studentList[i].style.display = 'inherit';
+    }
+  }
+}
 
 
 
@@ -20,9 +36,3 @@ FSJS project 2 - List Filter and Pagination
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
-
-
-
-
-
-
