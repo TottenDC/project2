a610c62bcdc7aa4d72fe2b71ffafec77ac52b4e5
Daniel Totten <TottenDC@gmail.com>
2018-09-08 19:31:01 -0400

    Finished search bar and auto-search features

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ FSJS project 2 - List Filter and Pagination
 const arrayOfStudents = document.querySelectorAll('.student-item');
 const pageDiv = document.querySelector('.page');
 const headerDiv = document.querySelector('.page-header');
+let searchResults = [];
 
 /*
 Show/Hide Student function
@@ -59,16 +60,32 @@ paginationDiv.addEventListener('click', (event) => {
   displayStudents(arrayOfStudents, pageNumber);
 });
 
+//Add search feature. Relatively complicated, comments are placed above line.
+
 //Create search bar and search button
 const searchBarDiv = document.createElement('div');
 searchBarDiv.className = 'student-search';
 searchBarDiv.innerHTML = '<input placeholder="Search for students..."> <button>Search</button>';
 headerDiv.appendChild(searchBarDiv);
 
-const searchButton = document.querySelector('.student-search button');
-searchButton.addEventListener('click', () => {
+//Create search function
+const searchFunction = () => {
+//Clear previous results
+  searchResults = [];
+  const errorRemoval = document.querySelector('.noResultMessage');
+  if (errorRemoval) {
+  errorRemoval.parentNode.removeChild(errorRemoval);
+  }
+/*
+Search 'meat-and-potatoes'
+1. Grab input and store in variables
+2. Loop through all students.
+  -Turn off display
+  -Grab student name and email
+  -Create a regular expression from input so can use .test method
+  -Add matches to new array
+*/
   let searchInput = document.querySelector('.student-search input').value.toLowerCase();
-  let searchResults = [];
   for (i=0; i<arrayOfStudents.length; i++) {
     arrayOfStudents[i].style.display = 'none';
     let name = arrayOfStudents[i].querySelector('h3').textContent;
@@ -78,5 +95,26 @@ searchButton.addEventListener('click', () => {
       searchResults.push(arrayOfStudents[i]);
     }
   }
+//Create an error message if no results are found
+  if (searchResults.length === 0) {
+    let messageElement = document.createElement('h2');
+    messageElement.className = 'noResultMessage'
+    messageElement.textContent = 'Sorry, no results found.';
+    pageDiv.appendChild(messageElement);
+  }
+//Remove old buttons
+  const buttonDiv = document.querySelector('.pagination');
+  buttonDiv.parentNode.removeChild(buttonDiv);
+//Add buttons related to search results
+  addPageButtons(searchResults);
+//Display the results
   displayStudents(searchResults, 1);
-})
+};
+
+//Create seach button functionality
+const searchButton = document.querySelector('.student-search button');
+searchButton.addEventListener('click', function () { searchFunction () });
+
+//Create a 'keyup' event to 'autosearch'
+const searchText = document.querySelector('.student-search input');
+searchText.addEventListener('keyup', function () { searchFunction () });
