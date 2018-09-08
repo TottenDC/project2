f3dead98b8de38f01e0ba4fa08b3d32d96cf8812
Daniel Totten <TottenDC@gmail.com>
2018-09-08 13:44:00 -0400

    Create addPageButton function
    Tested

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ FSJS project 2 - List Filter and Pagination
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const arrayOfStudents = document.querySelectorAll('.student-item');
-
+const pageDiv = document.querySelector('.page');
 
 /*
 Show/Hide Student function
@@ -14,7 +14,7 @@ Parameters: an array of objects containing student information / the current pag
 2. Display only 10 students based on page # (Ex: page 3 is (i=20; i<=29; i=i+1))
   - If statement avoids JS error when i > array.length
 */
-function displayStudents (studentList, page) {
+const displayStudents = (studentList, page) => {
   for (i=0; i<studentList.length; i++) {
     studentList[i].style.display = 'none';
   }
@@ -30,8 +30,18 @@ function displayStudents (studentList, page) {
 
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
-
-
+const addPageButtons = studentList => {
+  const buttonDiv = document.createElement('div');
+  buttonDiv.className = 'pagination';
+  const buttonUl = document.createElement('ul');
+  buttonDiv.appendChild(buttonUl);
+  for (i=0; i<(Math.ceil(studentList.length/10)); i++) {
+    let buttonLi = document.createElement('li');
+    buttonLi.innerHTML = `<a href='#'>${i+1}</a>`;
+    buttonUl.appendChild(buttonLi);
+  }
+  pageDiv.appendChild(buttonDiv);
+}
 
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
